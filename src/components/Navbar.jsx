a229d5a5e1e412751939e714d1ba7a8003a1eddf
MichBKT <michel.banckaert@epitech.eu>
2025-03-02 10:11:07 +0100

    [FIX] some visuals pb + indentation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,23 @@
 import Workspace from "./icons/Workspace.jsx";
+import Notification from "./icons/Notification.jsx";
+import Person from "./icons/Person.jsx";
 
 export default function Navbar() {
     return (
         <>
-            <div className={"flex w-full h-20 relative bg-white dark:bg-gray-900 content-center"}>
-                <div className={"p-4 w-full justify-items-end flex font-bold text-black dark:text-white text-2xl gap-4"}>
+            <div className={"flex w-full h-20 relative bg-white dark:bg-gray-900 duration-1000 content-center border-b-2 border-gray-200 dark:border-gray-700"}>
+                <div className={"p-6 w-full justify-items-end flex font-bold text-black dark:text-gray-300 text-xl gap-4"}>
                     <Workspace color={"#ECB500"}/>
                     Espace 1
+                </div>
+                <div className={"w-full flex flex-row items-center justify-end"}>
+                    <Notification />
+                </div>
+                <div className={"p-2 pl-6 w-100 text-black dark:text-gray-300 items-center flex flex-row justify-end"}>
+                    Bonjour John
+                </div>
+                <div className={"p-2 px-6 w-32 cursor-pointer text-black dark:text-gray-500 items-center flex flex-row justify-end"}>
+                    <Person />
                 </div>
             </div>
         </>
