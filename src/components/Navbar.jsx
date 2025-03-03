3a61db4077bf0a082c9c1fa5cdc63c555ba85bbe
MichBKT <michel.banckaert@epitech.eu>
2025-03-03 02:10:18 +0100

    [ADD] greeting based on current hour

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,37 @@
 import Workspace from "./icons/Workspace.jsx";
 import Notification from "./icons/Notification.jsx";
 import Person from "./icons/Person.jsx";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+    const [greeting, setGreeting] = useState("Bonjour");
+
+    useEffect(() => {
+        const currentHour = new Date().getHours();
+        if (currentHour < 6 || currentHour >= 18) {
+            setGreeting("Bonsoir");
+        } else {
+            setGreeting("Bonjour");
+        }
+    }, []);
+
     return (
         <>
-            <div className={"flex w-full h-20 relative bg-white dark:bg-gray-900 duration-1000 content-center border-b-2 border-gray-200 dark:border-gray-700"}>
-                <div className={"p-6 w-full justify-items-end flex font-bold text-black dark:text-gray-300 text-xl gap-4"}>
+            <div className="flex h-18 w-full bg-white dark:bg-gray-900 duration-1000 content-center border-b-2 border-gray-200 dark:border-gray-700 z-10 gap-4">
+                <div className=" w-full max-md:w-0 md:ml-32 lg:justify-center justify-end flex relative items-center font-bold text-black dark:text-gray-300 min-sm:text-xs lg:text-xl gap-0 md:gap-4">
                     <Workspace color={"#ECB500"}/>
-                    Espace 100
+                    Espace 1
                 </div>
-                <div className={"w-full flex flex-row items-center justify-end"}>
+                <div className="w-full max-md:ml-2 flex items-center justify-end">
                     <Notification />
                 </div>
-                <div className={"p-2 pl-6 w-100 text-black dark:text-gray-300 items-center flex flex-row justify-end"}>
-                    Bonjour John
+                <div className="max-md:invisible max-md:w-0 w-72 flex p-2 text-black dark:text-gray-300 items-center justify-end">
+                    {greeting} John
                 </div>
-                <div className={"p-2 pl-6 w-32 text-black dark:text-gray-500 items-center flex flex-row text-xl justify-end"}>
+                <div className="p-2 px-6 text-black dark:text-gray-500 flex items-center text-xl">
                     <Person />
                 </div>
             </div>
         </>
-    )
+    );
 }
