272895b4ea4dc0ba8fe9bfe535921f6b1da151f6
Ilyes <ilyes.benkerri27@gmail.com>
2025-03-31 11:21:09 +0200

    Fix Login

--- src/Login.jsx
+++ src/Login.jsx
@@ -4,9 +4,14 @@ import BrandWhiteIcon from "../src/components/icons/BrandWhiteIcon";
 import BrandIcon from "../src/components/icons/BrandIcon";
 import ToggleOnDarkMode from "../src/components/icons/ToggleOnDarkMode.jsx";
 import ToggleOffDarkMode from "../src/components/icons/ToggleOffDarkMode.jsx";
+import { Eye, EyeOff } from "lucide-react";
 
 export default function Login() {
     const [darkMode, setDarkMode] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [rememberMe, setRememberMe] = useState(false);
 
     useEffect(() => {
         if (Cookies.get('dark') === 'true') {
@@ -14,8 +19,6 @@ export default function Login() {
             setDarkMode(true);
         }
     }, []);
-
-
 
     const toggleDarkMode = () => {
         const newDarkMode = !darkMode;
@@ -61,10 +64,67 @@ export default function Login() {
                         Connexion
                     </h1>
                     <p className="opacity-60 text-center justify-start text-slate-800 dark:text-white text-base font-normal font-['Nunito'] leading-loose">
-                        Connectez-vous avec votre compte Trello pour continuer
+                        Veuillez vous connecter pour continuer avec votre compte
                     </p>
                 </div>
+                <div>
+                    <div className="relative my-4 w-xs">
+                        <label className="block justify-start text-slate-800 dark:text-white text-sm font-semibold font-['Nunito'] leading-snug" htmlFor="email">Email</label>
+                        <input 
+                            className="block w-full self-stretch text-slate-800 dark:text-white opacity-100 px-4 py-2.5 bg-purple-100 dark:bg-black/40 rounded-[10px] justify-start items-center gap-2.5" 
+                            type="email" 
+                            id="email"
+                            placeholder="user@domaine.com"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="relative my-4 w-xs">
+                        <label className="block justify-start text-slate-800 dark:text-white text-sm font-semibold font-['Nunito'] leading-snug" htmlFor="password">Mot de passe</label>
+                        <div className="relative">
+                            <input
+                                id="password"
+                                type={showPassword ? "text" : "password"}
+                                placeholder="****************"
+                                className="block w-full self-stretch text-slate-800 dark:text-white opacity-100 px-4 py-2.5 bg-purple-100 dark:bg-black/40 rounded-[10px] justify-start items-center gap-2.5"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <button
+                                type="button"
+                                className="absolute inset-y-0 right-3 flex items-center text-gray-600 hover:text-gray-900"
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                            </button>
+                        </div>
+                    </div>
+                    <div className="relative my-4 w-xs">
+                        <div className="inline-flex justify-start items-center gap-2.5">
+                            <input 
+                                type="checkbox" 
+                                id="rememberMe"
+                                checked={rememberMe}
+                                onChange={(e) => setRememberMe(e.target.checked)}
+                            />
+                            <label htmlFor="rememberMe" className="opacity-90 text-center justify-start text-slate-800 dark:text-white text-base font-normal font-['Nunito'] leading-snug">Rester connecté</label>
+                        </div>
+                    </div>
+                    <div className="relative my-4 w-xs">
+                        <button className="btn w-full relative overflow-hidden h-10 p-3 bg-purple-600 rounded-[400px] inline-flex justify-center items-center gap-2.5 text-white cursor-pointer hover:bg-purple-500 transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                            Connexion
+                        </button>
+                    </div>
+                </div>
+                <div>
+                    <span className="block justify-start text-slate-800 dark:text-white text-sm font-semibold font-['Nunito'] leading-snug underline">Mot de passe oublié ?</span>
+                </div>
                 <div className="relative my-4 w-xs">
+                    <div className="flex items-center justify-center my-4">
+                        <div className="flex-1 border-t border-gray-300"></div>
+                        <span className="px-4 text-gray-500">ou</span>
+                        <div className="flex-1 border-t border-gray-300"></div>
+                    </div>
                     <button 
                         onClick={handleTrelloLogin}
                         className="btn w-full relative overflow-hidden h-10 p-3 bg-[#0079BF] rounded-[400px] inline-flex justify-center items-center gap-2.5 text-white cursor-pointer hover:bg-[#026AA7] transition-all duration-300 hover:-translate-y-1 hover:shadow-lg"
